1d21c5613716c97632221c08022f6647e2a483fc
Emanuel151100 <61050024+Emanuel151100@users.noreply.github.com>
2020-03-18 14:23:05 +0100

    Update Menu.c
    He quitado letras como la ñ, y palabras con tildes .

--- Menu.c
+++ Menu.c
@@ -12,19 +12,19 @@ void main ()
 	switch (x)
 	{
 		case 1:
-			printf("Excelente,comencemos con la encriptación\n");
+			printf("Excelente,comencemos con la encriptacion\n");
 			break;
 		case 2:
 			printf("Excelente,vamos a ver de que trata ese mensaje...\n");
 			break;
 		case 3:
-			printf("Excelente,que le quiere transmitir al señor Stark?\n");
+			printf("Excelente,que le quiere transmitir al Sr.Stark?\n");
 			break;
 		case 4:
-			printf("Excelente,veamos que tiene para usted el sr Stark\n");
+			printf("Excelente,veamos que tiene para usted el Sr.Stark\n");
 			break;
 		case 5:
-			printf("Muy bien ...,hasta la pro xima\n");
+			printf("Muy bien ...,hasta la proxima\n");
 			break;
 	    default:
 	    	printf("Parece que el viaje de regreso desde Vormir no le ha sentado muy bien ...");
