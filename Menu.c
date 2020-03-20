936d01de42fe5d9b26e5f79a185256f74dd26108
Adriang2211 <adrian.gomezg@alumnos.upm.es>
2020-03-20 11:53:06 +0100

    Añadido bucle
    Añadido un bucle para que el programa se siga ejecutando hasta que el usuario decida salir.
    Añadidas tabulaciones en todas las líneas de dentro del bucle para respetar el formato y que se vean mejor las línas que se encuentran en el bucle. (Por este motivo aparecen cambios la mayor parte de las líneas).
    (Solo se han añadido las líneas 5, 6, 7, 31 y 35).

--- Menu.c
+++ Menu.c
@@ -2,31 +2,36 @@
 void main ()
 {
 	int x;
-	printf("Bienvenido me llamo Jarvis , que desea hacer?\n");//Le doy la bienvenida y le pregunto que desea hacer
-	printf("1.Encriptar\n");
-	printf("2.Desencriptar\n");
-	printf("3.Enviar\n");
-	printf("4.Recibir\n");
-	printf("5.Salir\n");//Le presento las opciones a seleccionar
-	scanf("%i",&x);
-	switch (x)
-	{
-		case 1:
-			printf("Excelente,comencemos con la encriptacion\n");
-			break;
-		case 2:
-			printf("Excelente,vamos a ver de que trata ese mensaje...\n");
-			break;
-		case 3:
-			printf("Excelente,que le quiere transmitir al Sr.Stark?\n");
-			break;
-		case 4:
-			printf("Excelente,veamos que tiene para usted el Sr.Stark\n");
-			break;
-		case 5:
-			printf("Muy bien ...,hasta la proxima\n");
-			break;
-	    default:
-	    	printf("Parece que el viaje de regreso desde Vormir no le ha sentado muy bien ...");
+	//El programa se ejecuta indefinidamente hasta que el usuario decide salir
+	_Bool continuar = 0;
+	while (continuar==0){
+		printf("Bienvenido me llamo Jarvis , que desea hacer?\n");//Le doy la bienvenida y le pregunto que desea hacer
+		printf("1.Encriptar\n");
+		printf("2.Desencriptar\n");
+		printf("3.Enviar\n");
+		printf("4.Recibir\n");
+		printf("5.Salir\n");//Le presento las opciones a seleccionar
+		scanf("%i",&x);
+		switch (x)
+		{
+			case 1:
+				printf("Excelente,comencemos con la encriptacion\n");
+				break;
+			case 2:
+				printf("Excelente,vamos a ver de que trata ese mensaje...\n");
+				break;
+			case 3:
+				printf("Excelente,que le quiere transmitir al Sr.Stark?\n");
+				break;
+			case 4:
+				printf("Excelente,veamos que tiene para usted el Sr.Stark\n");
+				break;
+			case 5:
+				printf("Muy bien ...,hasta la proxima\n");
+				continuar =1;
+				break;
+		    default:
+		    	printf("Parece que el viaje de regreso desde Vormir no le ha sentado muy bien ...");
+		}
 	}
 }
