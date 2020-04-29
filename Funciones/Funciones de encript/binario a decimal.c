95fb7ef938983aee016c2b1ada794fefd4d6267f
Emanuel151100 <61050024+Emanuel151100@users.noreply.github.com>
2020-04-29 17:22:28 +0200

    Update binario a decimal.c
    He cambiado la función debido a un error , he adjuntado el código completo para comprobar su debido funcionamiento

--- Funciones/Funciones de encript/binario a decimal.c
+++ Funciones/Funciones de encript/binario a decimal.c
@@ -1,13 +1,26 @@
-unsigned long long binarioADecimal(int T[11], int longitud);
-unsigned long long binarioADecimal(int T[11], int longitud)
+//Es una prueba para ver si funcionaba
+#include <stdio.h>
+unsigned long int binarioADecimal(int *p, int longitud);
+void main ()
+{
+ int i ,n,T[11],decimal;
+ scanf("%i",&n);
+ for(i=0;i<n;i++)
+ scanf("%i",&T[i]);
+ decimal=binarioADecimal(T,n);
+ printf("%i",decimal);
+}
+//Aqui acaba la prueba
+unsigned long int binarioADecimal(int *p, int longitud)//Funcion
 {
   unsigned long long decimal = 0;
-  int multiplicador = 1,i,caracterActual;
-  for ( i = longitud ; i >= 0; i--) 
+  int multiplicador = 1,i,numeroActual;
+  for ( i = longitud-1; i >= 0; i--) 
   {
-    caracterActual = T[i];
-    if (caracterActual == '1') {
-      decimal += multiplicador;
+    numeroActual = *(p+i);
+    if (numeroActual ==1) 
+	{
+      decimal+= multiplicador;
     }
     multiplicador = multiplicador * 2;
   }
