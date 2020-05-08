7db2e9ae55177cc73f81ffbad7b455ed5b54e1f6
JorgeBarahonaMonte <61050162+JorgeBarahonaMonte@users.noreply.github.com>
2020-05-08 19:27:09 +0200

    encriptador completo con las funciones

--- PROYECTO C/PRINCIPAL.c
+++ PROYECTO C/PRINCIPAL.c
@@ -2,16 +2,15 @@
 #include "Funciones.h"
 
 int main (){
-	int pswrdASCII[11], X[11], Y[11], Z[11],T[11],comodin2[11], comodin3[11], comodin4[11];
-	int iter=0, i=0;
-	char pswrd[12]; 
+	int pswrdASCII[11], X[11], Y[11], Z[11],T[11],U,D,C,comodin2[11], comodin3[11], comodin4[11];
+	int iter=0,j=0,i=0,x=0,y=0,z=0,cont;
+	char pswrd[12],p;
 	
 	fpswrd (pswrd); //FUNCIÓN (Contraseña).
-    printf("P");    			  
+   			  
 	while (i<12)
 	{
 	    Char_a_ASCII(pswrd,pswrdASCII); //FUNCIÓN (Pasar la contraseña de caracteres a ASCII)
-	        //pswrdASCII[i]= pswrd[i];
 	        printf("%d \t", pswrdASCII[i]);	
 	    
 		UDC (pswrdASCII, X, Y, Z); //FUNCIÓN (Pasamos ASCII a unidades,decenas y centenas)
@@ -67,4 +66,56 @@ int main (){
 	    i++;}
 	iter++;
 	}
+	
+	
+	while(i<12)
+	{
+		x=x*2+X[i];
+		y=y*2+Y[i];
+		z=z*2+Z[i];
+		i++;
+	}
+
+ //una vez hecho vamos a abrir el archivo, esta parte tenemos que tratarla todavÃ­a
+	FILE *txt,*txt2;
+
+	int *txtASCII;
+ 	txt=fopen ("encript.txt", "r");
+ 	if(txt==NULL)
+ 	{
+   	printf("Error");
+   	return(-1);
+	}                     
+
+	txtASCII=malloc(sizeof(txt));
+	if (txtASCII==NULL){
+ 		printf("Error en asignacion malloc");
+ 		exit(-1);
+ 	}
+ 	txt2=fopen("encript2.txt","w");
+	if(txt2==NULL)
+	{
+	printf("Error txt2");
+	}
+	printf("procediendo a leer por segunda vez");//Rewind
+  	while ((p=fgetc(txt))!=EOF){
+  	printf("pq no funcionas");
+  	if(j==12)
+  	j=0;
+  	*(txtASCII+i)=p;
+	*(txtASCII+i)+=pswrdASCII[j];
+	while(*(txtASCII+i)>255){                                 //Lo que estamos haciendo es acotar el numero obtenido para que sea positivo y [0,255]
+	*(txtASCII+i)-=255;
+	}
+	 	fprintf(txt2,"%c",*(txtASCII+i));  //Se puede asignar char a ascii //se imprime el valor entero como char
+	i++;                                   
+	j++;                                  
+	}
+	printf("hola");
+ 	fclose(txt2);
+ 	fclose(txt);
+ 	free(txtASCII);
+ 	return 0;
+	}
+		
 }
