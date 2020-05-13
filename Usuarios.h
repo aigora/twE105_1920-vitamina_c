df31aa18d692e268f3bb4e59a278cf171a50c1b2
Adriang2211 <61050000+Adriang2211@users.noreply.github.com>
2020-05-13 12:30:29 +0200

    Creación del fichero Usuarios.h
    Para el uso de la librería

---
+++ Usuarios.h
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "Usuarios.c" //Incluye el fichero con el programa con las funciones
+//constantes con los tamaños de algunos elementos del programa. Modificar según necesidades:
+#define MAX_USERS 100
+#define TAM_USER 30
+#define TAM_PASSWORD 30
+typedef struct{
+	char user[TAM_USER];
+	char password[TAM_PASSWORD];
+}usuarios;
+void nuevo (FILE *fichero, usuarios *new_user, int ultimo_registro, usuarios lista []);
+_Bool iniciar_sesion (FILE *fichero, usuarios usuarios_existentes[], int ultimo_registro); //El fichero es solo por si se elige crear un usuario
+int consulta_registros(FILE *fichero, usuarios vector[]); //Lee el fichero para averiguar el número de registros existentes (requiere como argumento el puntero al archivo) y además lee y guarda los datos en un vector dado
+_Bool comparar_cadenas (char *cadena1, char *cadena2, int l1);
+void espera (int intentos);
+int inicio_normal();
